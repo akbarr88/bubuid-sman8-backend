7722b5c5a022997c474b6447a5cded01aedfc8b9
kinxyzz <kinxyzz@gmail.com>
2024-05-15 20:11:44 +0700

    addimageuploadhandler

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
- const express = require('express');
-const app = express()
-const allRoutes = require("./routes")
-const cors = require('cors');
+const express = require("express");
+const app = express();
+const allRoutes = require("./routes");
+const cors = require("cors");
+const path = require("path");
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const { cloudinary } = require("./config/cloudinary");
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "imagebackend",
+    format: async (req, file) => "jpg",
+    public_id: (req, file) => `${file.originalname.split(".")[0]}${Date.now()}`,
+  },
+});
 
-app.use(cors())
-app.use(express.json({ limit: '50mb' }))
-app.use(allRoutes)
+const upload = multer({ storage });
 
-
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static(path.join(__dirname, "public")));
+app.post("/upload", upload.single("file"), (req, res) => {
+  try {
+    const finalImageUrl = req.file.path;
+    res.json({ status: "succes", imageUrl: finalImageUrl });
+  } catch (error) {
+    res.json({ status: "failed", error: error });
+  }
+});
+app.use(allRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server Berjalan di PORT " + PORT);
-})
+  console.log("Server Berjalan di PORT " + PORT);
+});
--- controllers/artikel.controller.js
+++ controllers/artikel.controller.js
@@ -1,72 +1,90 @@
-const {Artikel} = require("../models")
+const { Artikel } = require("../models");
 
 module.exports = {
-    getAllArtikel: async (req, res) => {
-    const artikels = await Artikel.findAll()
+  getAllArtikel: async (req, res) => {
+    const artikels = await Artikel.findAll();
 
-    res. json ({
-        message: "berhasil mendapatkan data Artikel",
-        data: artikels
-    })
-    },
+    res.json({
+      message: "berhasil mendapatkan data Artikel",
+      data: artikels,
+    });
+  },
 
-    getArtikelById: async (req, res) => {
-        const {id}= req.params
-        const artikel = await Artikel.findOne({
-            where:{id:id}
-        })
-    
-        res. json ({
-            message: "berhasil mendapatkan data id",
-            data: artikel
-        }) 
-    },
+  getArtikelById: async (req, res) => {
+    const { id } = req.params;
+    const artikel = await Artikel.findOne({
+      where: { id: id },
+    });
 
-    addNewArtikel: async (req, res) => {
-        const data = req.body;
-        await Artikel.create(data);
-        res.status(201).json({
-          message: "Berhasil menambahkan Artikel",
-        });
-      },
+    res.json({
+      message: "berhasil mendapatkan data id",
+      data: artikel,
+    });
+  },
 
-      deleteArtikel: async (req, res) => {
-        try {
-          const { id } = req.params;
-          await Artikel.destroy({
-            where: {
-              id : id
-            }
-          });
-          res.json({
-            message : "Data Artikel Berhasil dihapus"
-          })
-        } catch (error) {
-          res.status(500).json({
-            message: "Gagal menghapus data Artikel",
-            error: error.message,
-          });
-        }
-      },
+  addNewArtikel: async (req, res) => {
+    const {
+      highlight_isi = "headline",
+      judul = "judul",
+      isi = "content",
+      gambar,
+      penulis = "penulis",
+      pekerjaan = "pekerjaan",
+    } = await req.body;
 
-      updateArtikel: async (req, res) => {
-        try {
-          const { id } = req.params;
-          const data = req.body;
-          await Artikel.update(data, {
-            where: {
-              id: id,
-            },
-          });
-    
-          res.status(201).json({
-            message: "Berhasil mengupdate data Artikel",
-          });
-        } catch (error) {
-          res.status(500).json({
-            message: "Gagal mengupdate data Artikel",
-            error: error.message,
-          });
-        }
-      },
-}
+    const data = {
+      highlight_isi,
+      judul,
+      isi,
+      gambar,
+      penulis,
+      pekerjaan,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    await Artikel.create(data);
+    res.status(201).json({
+      message: "Berhasil menambahkan Artikel",
+    });
+  },
+
+  deleteArtikel: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Artikel.destroy({
+        where: {
+          id: id,
+        },
+      });
+      res.json({
+        message: "Data Artikel Berhasil dihapus",
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Gagal menghapus data Artikel",
+        error: error.message,
+      });
+    }
+  },
+
+  updateArtikel: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const data = req.body;
+      await Artikel.update(data, {
+        where: {
+          id: id,
+        },
+      });
+
+      res.status(201).json({
+        message: "Berhasil mengupdate data Artikel",
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Gagal mengupdate data Artikel",
+        error: error.message,
+      });
+    }
+  },
+};
